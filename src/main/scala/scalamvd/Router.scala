84eb93d40c15566dff7c812c0071e3e94e2f11e5
fedesilva <fede.silva@gmail.com>
2013-08-29 18:18:27 -0300

    cleanup

--- src/main/scala/scalamvd/Router.scala
+++ src/main/scala/scalamvd/Router.scala
@@ -2,28 +2,33 @@ package scalamvd
 
 import spray.util._
 import spray.routing._
+import akka.actor.{Props, ActorRef}
+import scalamvd.meetup.{GroupInfo, MeetupApiClient}
 
-// we don't implement our route structure directly in the service actor because
-// we want to be able to test it independently, without having to spin up an actor
-class RootService
+object Router {
+  def apply( group: GroupInfo ) =
+    Props(classOf[Router], group)
+
+}
+
+class Router(val group: GroupInfo)
 extends HttpServiceActor
-  with SiteRoutes   // Static routes
+  with SiteRoutes       // Static routes
   with MeetupRoutes
   with SprayActorLogging {
+
 
   // this actor only runs our routes
   def receive = runRoute(siteRoute ~ meetupRoutes)
 
+
 }
 
-/** Static routes and a test entry point.
-  *
-  *  Defined separately to enable testing without an actor system.
-  *
-  */
+
+/** Static routes and a test entry point. */
 trait SiteRoutes extends HttpService {
 
-  def siteRoute = testRoute ~ staticRoute
+  def siteRoute = staticRoute ~ testRoute
 
   def staticRoute =
     path("")( getFromResource("web/index.html") ) ~
